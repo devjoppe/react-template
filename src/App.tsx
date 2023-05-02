f3c2be03c576e4558893f1bdfe89b6b8728d18e8
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-05-02 09:59:46 +0200

    Template completed

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+// Import icon assets
+import reactLogo from './assets/icons/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+    </div>
+  )
+}
+
+export default App
